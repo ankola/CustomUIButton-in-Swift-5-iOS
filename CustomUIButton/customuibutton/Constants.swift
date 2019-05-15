2addb9b62f0ba9ee499dfc3cfa6169b97adc0cd0
Savan Ankola <38508703+savanankola@users.noreply.github.com>
2019-05-15 10:54:41 +0530

    Add files via upload

---
+++ CustomUIButton/customuibutton/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  CustomUIButton
+//
+//  Created by Sean Allen on 2/2/19.
+//  Copyright © 2019 Sean Allen. All rights reserved.
+//
+
+import UIKit
+
+enum Colors {
+    static let coolBlue = UIColor(red: 42/255, green: 126/255, blue: 254/255, alpha: 1.0)
+}
